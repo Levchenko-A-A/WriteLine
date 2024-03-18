ea54a309ecfba5a9ec321158708eca7e3da9719c
lev_a <lev_a@LAPTOP-CDOFAQH2>
2024-03-18 22:36:23 +0200

    Добавьте файлы проекта.

---
+++ WriteLine/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace WriteLine
+{
+    internal class Program
+    {
+        string lineInput;
+        while ((lineInput = Console.ReadLine()) != null)
+        {
+            string[] fields = lineInput.Split(new char[] { '\t' });
+        bool isFirstField = true;
+            foreach (var item in fields)
+            {
+                if (isFirstField)
+                    isFirstField = false;
+                else
+                    Console.Write(',');
+
+                // If the field represents a boolean, replace with a numeric representation.
+                bool itemBool;
+                if (Boolean.TryParse(item, out itemBool))
+                    Console.Write(Convert.ToByte(itemBool));
+                else
+                    Console.Write(item);
+            }
+    Console.WriteLine();
+}
